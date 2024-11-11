cb5442b0d668b787c4f61b707bb4ebadb770b19e
AzureAD\LucasPerussi-Wetalki <perussilucas@hotmail.com>
2024-11-11 16:46:57 -0300

    implementação de requests

--- src/routes/requests/index.ts
+++ src/routes/requests/index.ts
@@ -2,63 +2,120 @@ import { Router } from 'express';
 import { validateJWT } from '../../middlewares/JWTVerifier';
 import prisma from '../../prismaClient'; // Adjust the path as necessary
 import { Logger } from '../../middlewares/logger';
-import { numberGenerator } from '../../middlewares/randomCodeGenerator';
+import { codeGenerator, numberGenerator } from '../../middlewares/randomCodeGenerator';
 
 export const routerRequests = Router()
 
 routerRequests.get('/', (req, res) => res.send('API de Solicitações'))
 
-// routerRequests.post('/new', validateJWT, async (req, res) => {
-//     let { titulo, descricao, aluno, servidor_responsavel, professor_avaliador, tipo_solicitacao } = req.body;
-//     let identificador = await Number(numberGenerator(20));
+routerRequests.post('/new', validateJWT, async (req, res) => {
+    let { titulo, descricao, aluno, servidor_responsavel, professor_avaliador, tipo_solicitacao } = req.body;
+    let numero = await Number(numberGenerator(8));
+    let identificador = await codeGenerator(60);
 
-//     try {
-//         const field = await prisma.processo.create({
-//             data: {
-//                 titulo,
-//                 descricao,
-//                 aluno,
-//                 servidor_responsavel,
-//                 tipo_solicitacao,
-//                 data_abertura: '',
-//                 identificador, 
-//                 etapa_atual: 1,
-//                 professor_avaliador
-//             }
-//         })
-//         Logger(`POST - FIELDS - new-type`, JSON.stringify(field), "success");
-//         res.status(200).send(JSON.stringify(field));
-//     } catch (error) {
-//         if (error instanceof Error) {
-//             console.error('Erro ao criar estágio:', error.message);
-//             Logger(`POST - FIELDS - new-type`, error.message, "error");
-//             res.status(500).send({ message: 'Erro ao criar estágio', error: error.message });
-//         } else {
-//             console.error('Erro ao criar estágio: Erro desconhecido');
-//             Logger(`POST - FIELDS - new-type`, "Erro desconhecido", "error");
-//             res.status(500).send({ message: 'Erro ao criar estágio', error: 'Erro desconhecido' });
-//         }
-//     }
-// });
+    try {
+        const field = await prisma.processo.create({
+            data: {
+                titulo,
+                descricao,
+                aluno,
+                tipo_solicitacao,
+                data_abertura: '',
+                identificador, 
+                numero,
+                etapa_atual: 1
+            }
+        })
+        Logger(`POST - REQUESTS - new`, JSON.stringify(field), "success");
+        res.status(200).send(JSON.stringify(field));
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Erro ao criar estágio:', error.message);
+            Logger(`POST - REQUESTS - new`, error.message, "error");
+            res.status(500).send({ message: 'Erro ao criar estágio', error: error.message });
+        } else {
+            console.error('Erro ao criar estágio: Erro desconhecido');
+            Logger(`POST - REQUESTS - new`, "Erro desconhecido", "error");
+            res.status(500).send({ message: 'Erro ao criar estágio', error: 'Erro desconhecido' });
+        }
+    }
+});
 
-// routerFields.get('/field-type-id/:id', validateJWT, async (req, res) => {
-//     const id = Number(req.params.id);
-//     try {
-//         const steps = await prisma.tipos_campos.findFirst({
-//             where: {
-//                 id: id
-//             }
-//         })
+routerRequests.patch('/add-teacher', validateJWT, async (req, res) => {
+    const { id, professor } = req.body;
 
-//         if (steps) {
-//             Logger(`GET - FIELDS - field-type-id/${id}`, `200 - Found and Authorized`, "success");
-//             res.status(200).send(JSON.stringify(steps));
-//         } else {
-//             Logger(`GET - FIELDS - field-type-id/${id}`, `404 - Not Found`, "error");
-//             res.status(404).send({ error: true, message: 'Field type not found!' });
-//         }
-//     } catch (error) {
-//         Logger(`GET - FIELDS - field-type-id/${id}`, `Error fetching requested Field type. ${JSON.stringify(error)} `, "error");
-//         res.status(500).json({ message: 'Error fetching requested Field type.' });
-//     }
-// });
+    try {
+        const field = await prisma.processo.update({
+            where: {
+                id
+            },
+            data: {
+                professor_avaliador: professor,
+            }
+        });
+        
+        Logger(`PATCH - REQUESTS - add-teacher`, JSON.stringify(field), "success");
+        res.status(200).json(field); 
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Erro ao atualizar professor avaliador:', error.message);
+            Logger(`PATCH - REQUESTS - add-teacher`, error.message, "error");
+            res.status(500).json({ message: 'Erro ao atualizar professor avaliador', error: error.message });
+        } else {
+            console.error('Erro ao atualizar professor avaliador: Erro desconhecido');
+            Logger(`PATCH - REQUESTS - add-teacher`, "Erro desconhecido", "error");
+            res.status(500).json({ message: 'Erro ao atualizar professor avaliador', error: 'Erro desconhecido' });
+        }
+    }
+});
+
+routerRequests.patch('/add-server', validateJWT, async (req, res) => {
+    const { id, servidor } = req.body;
+
+    try {
+        const field = await prisma.processo.update({
+            where: {
+                id
+            },
+            data: {
+                servidor_responsavel: servidor,
+            }
+        });
+        
+        Logger(`PATCH - REQUESTS - add-server`, JSON.stringify(field), "success");
+        res.status(200).json(field); 
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Erro ao atualizar server avaliador:', error.message);
+            Logger(`PATCH - REQUESTS - add-server`, error.message, "error");
+            res.status(500).json({ message: 'Erro ao atualizar professor avaliador', error: error.message });
+        } else {
+            console.error('Erro ao atualizar server avaliador: Erro desconhecido');
+            Logger(`PATCH - REQUESTS - add-server`, "Erro desconhecido", "error");
+            res.status(500).json({ message: 'Erro ao atualizar server avaliador', error: 'Erro desconhecido' });
+        }
+    }
+});
+
+
+routerRequests.get('/all', validateJWT, async (req, res) => {
+    const id = Number(req.params.id);
+    try {
+        const steps = await prisma.processo.findFirst({
+            where: {
+                id: id
+            }
+        })
+
+        if (steps) {
+            Logger(`GET - REQUESTS - all/${id}`, `200 - Found and Authorized`, "success");
+            res.status(200).send(JSON.stringify(steps));
+        } else {
+            Logger(`GET - REQUESTS - all/${id}`, `404 - Not Found`, "error");
+            res.status(404).send({ error: true, message: 'Field type not found!' });
+        }
+    } catch (error) {
+        Logger(`GET - REQUESTS - all/${id}`, `Error fetching requested Field type. ${JSON.stringify(error)} `, "error");
+        res.status(500).json({ message: 'Error fetching requested Field type.' });
+    }
+});
