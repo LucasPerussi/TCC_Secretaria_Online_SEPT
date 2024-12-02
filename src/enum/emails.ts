7cbc8f19136b38a329107e06e39677a28d2947da
AzureAD\LucasPerussi-Wetalki <perussilucas@hotmail.com>
2024-12-02 01:56:22 -0300

    cores melhores

--- src/enum/emails.ts
+++ src/enum/emails.ts
@@ -12,11 +12,10 @@ export enum EmailTypes {
 }
 
 export const EmailTypeInfo: Record<EmailTypes, EmailType> = {
-    [EmailTypes.ACCOUNT_CREATION]: { id: 1, nome: "Criação de Conta", cor: "#ffffff" },
-    [EmailTypes.LOGIN]: { id: 2, nome: "Login", cor: "#0000ff" },
-    [EmailTypes.TICKET_OPEN]: { id: 3, nome: "Chamado registrado", cor: "#ff0f0f" },
-    [EmailTypes.PASSWORD_CHANGE]: { id: 4, nome: "Alteração de Senha", cor: "#ff0fb7" }
+    [EmailTypes.ACCOUNT_CREATION]: { id: 1, nome: "Criação de Conta", cor: "#1E90FF" },
+    [EmailTypes.LOGIN]: { id: 2, nome: "Login", cor: "#20B2AA" },
+    [EmailTypes.TICKET_OPEN]: { id: 3, nome: "Chamado registrado", cor: "#FA8072" },
+    [EmailTypes.PASSWORD_CHANGE]: { id: 4, nome: "Alteração de Senha", cor: "#00BFFF" }
 };
-
 
 
--- src/enum/timeline.ts
+++ src/enum/timeline.ts
@@ -12,11 +12,9 @@ export enum TimelineTypes {
 }
 
 export const TimelineTypeInfo: Record<TimelineTypes, TimelineType> = {
-    [TimelineTypes.ACCOUNT_CREATION]: { id: 1, nome: "Criação de Conta", cor: "#ffffff" },
-    [TimelineTypes.LOGIN]: { id: 2, nome: "Login", cor: "#0000ff" },
-    [TimelineTypes.TICKET_OPEN]: { id: 3, nome: "Chamado registrado", cor: "#ff0f0f" },
-    [TimelineTypes.PASSWORD_CHANGE]: { id: 4, nome: "Alteração de Senha", cor: "#ff0fb7" }
+    [TimelineTypes.ACCOUNT_CREATION]: { id: 1, nome: "Criação de Conta", cor: "#1E90FF" },
+    [TimelineTypes.LOGIN]: { id: 2, nome: "Login", cor: "#20B2AA" },
+    [TimelineTypes.TICKET_OPEN]: { id: 3, nome: "Chamado registrado", cor: "#FA8072" },
+    [TimelineTypes.PASSWORD_CHANGE]: { id: 4, nome: "Alteração de Senha", cor: "#00BFFF" }
 };
-
-
 
