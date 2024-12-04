983a17019cdf639451d3c4de4453c633c85205ff
AzureAD\LucasPerussi-Wetalki <perussilucas@hotmail.com>
2024-12-04 01:12:07 -0300

    endpoints para stages

--- src/enum/proccessStages.ts
+++ src/enum/proccessStages.ts
@@ -28,85 +28,85 @@ export const ProcessoEtapaInfo: Record<ProcessoEtapas, StageType> = {
     [ProcessoEtapas.ABERTO_PENDENTE]: {
         id: 1,
         nome: "Aberto / Pendente",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#FFA500", // Laranja
     },
     [ProcessoEtapas.EM_ANALISE]: {
         id: 2,
         nome: "Em Análise",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#FFFF00", // Amarelo
     },
     [ProcessoEtapas.ENCAMINHADO_PROFESSOR_RESPONSAVEL]: {
         id: 3,
         nome: "Encaminhado para Professor Responsável",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#0000FF", // Azul
     },
     [ProcessoEtapas.ENCAMINHADO_TECNICO_RESPONSAVEL]: {
         id: 4,
         nome: "Encaminhado para Técnico Responsável",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#00FFFF", // Ciano
     },
     [ProcessoEtapas.ENCAMINHADO_RESPONSAVEL]: {
         id: 5,
         nome: "Encaminhado para Responsável",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#800080", // Roxo
     },
     [ProcessoEtapas.EM_CONTESTACAO]: {
         id: 6,
         nome: "Em Contestação",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#FF4500", // Laranja avermelhado
     },
     [ProcessoEtapas.DEFERIDO]: {
         id: 7,
         nome: "Deferido",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#008000", // Verde
     },
     [ProcessoEtapas.INDEFERIDO]: {
         id: 8,
         nome: "Indeferido",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#FF0000", // Vermelho
     },
     [ProcessoEtapas.CONCLUIDO]: {
         id: 9,
         nome: "Concluído",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#32CD32", // Verde limão
     },
     [ProcessoEtapas.CANCELADO]: {
         id: 10,
         nome: "Cancelado",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#A9A9A9", // Cinza
     },
     [ProcessoEtapas.REVISADO]: {
         id: 11,
         nome: "Revisado",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#4169E1", // Azul Royal
     },
     [ProcessoEtapas.AGUARDANDO_DOCUMENTOS]: {
         id: 12,
         nome: "Aguardando Documentos",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#FFD700", // Dourado
     },
     [ProcessoEtapas.AGUARDANDO_APROVACAO]: {
         id: 13,
         nome: "Aguardando Aprovação",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#40E0D0", // Turquesa
     },
     [ProcessoEtapas.EM_RECURSO]: {
         id: 14,
         nome: "Em Recurso",
-        estimativaHoras: 0,
+        estimativaHoras: 24,
         cor: "#DC143C", // Carmesim
     },
 };
--- src/routes/steps/index.ts
+++ src/routes/steps/index.ts
@@ -37,14 +37,14 @@ routerSteps.post('/new', validateJWT, async (req, res) => {
 });
 
 
-routerSteps.delete('/remove-link-stage-to-request/:field/:processo', validateJWT, async (req, res) => {
+routerSteps.delete('/remove-link-stage-to-request/:stage/:processo', validateJWT, async (req, res) => {
     const stage = Number(req.params.stage);
     const processo = Number(req.params.processo);
 
     try {
         const removeField = await prisma.etapas_processo.deleteMany({
             where: {
-                tipo: stage,
+                id: stage,
                 proccessTypeId: processo,
             }
         });
@@ -89,6 +89,34 @@ routerSteps.post('/new-default', validateJWT, async (req, res) => {
             console.error('Erro ao criar estágio: Erro desconhecido');
             Logger(`POST - STEPS - new-default`, "Erro desconhecido", "error");
             res.status(500).send({ message: 'Erro ao criar estágio', error: 'Erro desconhecido' });
+        }
+    }
+});
+
+routerSteps.post('/new-customized', validateJWT, async (req, res) => {
+    let { nome, label, estimativaHoras, cor } = req.body;
+
+    try {
+        const step = await prisma.enum_etapas.create({
+            data: {
+                nome,
+                label,
+                estimativaHoras: Number(estimativaHoras),
+                cor,
+                padrao: 0
+            }
+        })
+        Logger(`POST - STEPS - new-customized`, JSON.stringify(step), "success");
+        res.status(200).send(JSON.stringify(step));
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Erro ao criar stage:', error.message);
+            Logger(`POST - STEPS - new-customized`, error.message, "error");
+            res.status(500).send({ message: 'Erro ao criar stage', error: error.message });
+        } else {
+            console.error('Erro ao criar stage: Erro desconhecido');
+            Logger(`POST - STEPS - new-customized`, "Erro desconhecido", "error");
+            res.status(500).send({ message: 'Erro ao criar stage', error: 'Erro desconhecido' });
         }
     }
 });
@@ -197,6 +225,27 @@ routerSteps.get('/all-default', validateJWT, async (req, res) => {
         }
     } catch (error) {
         Logger(`GET - STEPS - all-default`, `Error fetching requested Steps. ${JSON.stringify(error)} `, "error");
+        res.status(500).json({ message: 'Error fetching requested Step.' });
+    }
+});
+
+routerSteps.get('/all-customized', validateJWT, async (req, res) => {
+    try {
+        const steps = await prisma.enum_etapas.findMany({
+            where: {
+                padrao : 0
+            }
+        })
+
+        if (steps) {
+            Logger(`GET - STEPS - all-customized`, `200 - Found and Authorized`, "success");
+            res.status(200).send(JSON.stringify(steps));
+        } else {
+            Logger(`GET - STEPS - all-customized`, `404 - Not Found`, "error");
+            res.status(404).send({ error: true, message: 'Steps not found!' });
+        }
+    } catch (error) {
+        Logger(`GET - STEPS - all-customized`, `Error fetching requested Steps. ${JSON.stringify(error)} `, "error");
         res.status(500).json({ message: 'Error fetching requested Step.' });
     }
 });
